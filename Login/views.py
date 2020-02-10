e78159ccb6e39ce262e276d504aaab76c338c6d0
AleexDL <183380@ids.upchiapas.edu.mx>
2020-02-10 10:33:19 -0600

    example2

--- Login/views.py
+++ Login/views.py
@@ -4,6 +4,22 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import generics
+
+from Login.models import  Example2
+from Login.serializer import Example2Serializers
+
+class ExampleList2(APIView):
+    def get(self, request, format=None):
+        print("metodo get filter")
+        queryset= Example2.objects.filter(delete = False)
+        serializer= Example2Serializers(queryset, many=True)
+        return Response(serializer.data)
 
 class CustonAuthToken(ObtainAuthToken):
 
